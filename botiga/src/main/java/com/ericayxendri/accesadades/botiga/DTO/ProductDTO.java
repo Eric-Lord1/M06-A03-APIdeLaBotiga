c3634ff9e5745505af5fa91bf90b2470caae8c00
alex <alexvictorb@gmail.com>
2025-04-10 14:43:52 +0200

    ex5 fet he tret ids i datas

--- botiga/src/main/java/com/ericayxendri/accesadades/botiga/DTO/ProductDTO.java
+++ botiga/src/main/java/com/ericayxendri/accesadades/botiga/DTO/ProductDTO.java
@@ -1,6 +1,6 @@
 package com.ericayxendri.accesadades.botiga.DTO;
 
-import java.time.LocalDate;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,13 +10,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductDTO {
-    private Long id;
     private String name;
     private String description;
     private String company;
     private Float price;
     private Long units;
-    private LocalDate creationDate;
-    private LocalDate updateDate;
     private String subcategory;
 }
